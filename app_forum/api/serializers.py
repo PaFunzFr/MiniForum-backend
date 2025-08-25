92ab1efa5ef3b8c4c7c510d1b0980ae0051fed43
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-08-25 12:04:51 +0200

    Add url (HyperlinkedIdentityField) to PostSerializer

--- app_forum/api/serializers.py
+++ app_forum/api/serializers.py
@@ -1,12 +1,6 @@
 from app_forum.models import Post, Comment
 from rest_framework import serializers
 
-class PostSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'author', 'content', 'created_at']
-        read_only_fields = ['author', 'created_at']
 
 class CommentSerializer(serializers.ModelSerializer):
 
@@ -14,3 +8,12 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = ['id', 'author', 'text', 'created_at']
         read_only_fields = ['id', 'author', 'created_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    url = serializers.HyperlinkedIdentityField(view_name='post-detail', lookup_field='pk')
+    class Meta:
+        model = Post
+        fields = ['id','url', 'title', 'author', 'content', 'created_at', 'comments']
+        read_only_fields = ['author', 'created_at']
+
